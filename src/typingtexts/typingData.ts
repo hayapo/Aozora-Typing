fe239c313e599abbb5a420ba7eaf5fe78d034c58
hayapo <keep.forward1001@gmail.com>
2023-02-07 00:40:55 +0900

    [repo] タイピング文字列データを少し改修

---
+++ src/typingtexts/typingData.ts
@@ -0,0 +1,64 @@
+import { TypingDataType } from "../types/typingDataType"
+
+export const typingData: TypingDataType[] = [
+  {
+    id: 761,
+    title: "虞美人草",
+    author: "夏目漱石",
+    kanjiText: "「随分遠いね。元来どこから登るのだ」と一人がハンケチで額を拭きながら立ち留った。「どこかおれにも判然せんがね。どこから登ったって、同じ事だ。山はあすこに見えているんだから」と顔も体躯も四角に出来上った男が無雑作に答えた。",
+    hiraganaText: "「ずいぶんとおいね。がんらいどこからのぼるのだ」とひとりがはんけちでひたいをふきながらたちどまった。「どこかおれにもはんぜんせんがね。どこからのぼったって、おなじことだ。やまはあすこにみえているんだから」とかおもたいくもしかくにできあがったおとこがむぞうさにこたえた。",
+    wakatiRomajiText: "[ zui bun too i ne . gan rai do ko ka ra nobo ru no da ] to hito ri ga ha n ke ti de hitai wo hu ki na ga ra ta ti to doma tta . [ do ko ka ore ni mo han zen se n ga ne . do ko ka ra nobo tta tte , ona zi koto da . yama ha a su ko ni mi e te i ru n da ka ra ] to kao mo tai ku mo si kaku ni de ki aga tta otoko ga mu zou sa ni kota e ta .",
+    romajiText: "「zuibuntooine.ganraidokokaranoborunoda」tohitorigahanketidehitaiwohukinagaratatitodomatta.[dokokaorenimohanzensengane.dokokaranobottatte,onazikotoda.yamahaasukonimieteirundakara]tokaomotaikumosikakunidekiagattaotokogamuzousanikotaeta.",
+    url: "https://www.aozora.gr.jp/cards/000148/card761.html"
+  },
+  {
+    id: 773,
+    title: "こころ",
+    author: "夏目 漱石",
+    kanjiText: "私はその人を常に先生と呼んでいた。だからここでもただ先生と書くだけで本名は打ち明けない。これは世間を憚かる遠慮というよりも、その方が私にとって自然だからである。私はその人の記憶を呼び起すごとに、すぐ「先生」といいたくなる。",
+    hiraganaText: "わたしはそのひとをつねにせんせいとよんでいた。だからここでもただせんせいとかくだけでほんみょうはうちあけない。これはせけんをはばかるえんりょというよりも、そのほうがわたしにとってしぜんだからである。わたしはそのひとのきおくをよびおこすごとに、すぐ「せんせい」といいたくなる。",
+    wakatiRomajiText: "watasi ha so no hito wo tune ni sen sei to yo n de i ta . da ka ra ko ko de mo ta da sen sei to ka ku da ke de hon myou ha u ti a ke na i . ko re ha se ken wo haba karu en ryo to iu yo ri mo , so no hou ga watasi ni to tte si zen da ka ra de a ru . watasi ha so no hito no ki oku wo yo bi o ko su go to ni, su gu [ sen sei ] to i i ta ku na ru .",
+    romajiText: "watasihasonohitowotunenisenseitoyondeita.dakarakokodemotadasenseitokakudakedehonmyouhautiakenai.korehasekenwohabakaruenryotoiuyorimo,sonohougawatasinitottesizendakaradearu.watasihasonohitonokiokuwoyobiokosugotoni,sugu[sensei]toiitakunaru.",
+    url: "https://www.aozora.gr.jp/cards/000148/card773.html"
+  },
+  {
+    id: 799,
+    title: "夢十夜",
+    author: "夏目 漱石",
+    kanjiText: "こんな夢を見た。腕組をして枕元に坐っていると、仰向に寝た女が、静かな声でもう死にますと云う。女は長い髪を枕に敷いて、輪郭の柔らかな瓜実顔をその中に横たえている。真白な頬の底に温かい血の色がほどよく差して、唇の色は無論赤い。とうてい死にそうには見えない。",
+    hiraganaText: "こんなゆめをみた。うでぐみをしてまくらもとにすわっていると、あおむきにねたおんなが、しずかなこえでもうしにますという。おんなはながいかみをまくらにしいて、りんかくのやわらかなうりざねがおをそのなかによこたえている。まっしろなほおのそこにあたたかいちのいろがほどよくさして、くちびるのいろはむろんあかい。とうていしにそうにはみえない。",
+    wakatiRomajiText: "ko n na yume wo mi ta . ude gumi wo si te makura moto ni suwa tte i ru to 、ao muke ni ne ta onna ga , sizu ka na koe de mo u si ni ma su to i u . onna ha naga i kami wo makura ni si i te , rin kaku no yawa ra ka na uri zane gao wo so no naka ni yoko ta e te i ru . ma ssiro na hoo no soko ni atata ka i ti no iro ga ho do yo ku sa si te , kutibiru no iro ha mu ron aka i . to u te i si ni so u ni ha mi e na i .",
+    romajiText: "konnayumewomita.udegumiwositemakuramotonisuwatteiruto,aomukininetaonnaga,sizukanakoedemusinimasutoiu.onnahanagaikamiwomakuranisiite,rinkakunoyawarakanaurizanegaowosononakaniyokotaeteiru.massironahoonosokoniatatakaitinoirogahodoyokusasite,kutibirunoirohamuronakai.touteisinisounihamienai.",
+    url: "https://www.aozora.gr.jp/cards/000148/card799.html"
+  },
+  {
+    id: 789,
+    title: "吾輩は猫である",
+    author: "夏目 漱石",
+    kanjiText: "吾輩は猫である。名前はまだ無い。どこで生れたかとんと見当がつかぬ。何でも薄暗いじめじめした所でニャーニャー泣いていた事だけは記憶している。吾輩はここで始めて人間というものを見た。しかもあとで聞くとそれは書生という人間中で一番獰悪な種族であったそうだ。この書生というのは時々我々を捕えて煮て食うという話である。しかしその当時は何という考えもなかったから別段恐しいとも思わなかった。ただ彼の掌に載せられてスーと持ち上げられた時何だかフワフワした感じがあったばかりである。掌の上で少し落ちついて書生の顔を見たのがいわゆる人間というものの見始めであろう。この時妙なものだと思った感じが今でも残っている。",
+    hiraganaText: "わがはいはねこである。なまえはまだない。どこでうまれたかとんとけんとうがつかぬ。なんでもうすぐらいじめじめしたところでにゃーにゃーないていたことだけはきおくしている。わがはいはここではじめてにんげんというものをみた。しかもあとできくとそれはしょせいというにんげんちゅうでいちばんどうあくなしゅぞくであったそうだ。このしょせいというのはときどきわれわれをとらえてにてくうというはなしである。しかしそのとうじはなんというかんがえもなかったからべつだんこわしいともおもわなかった。ただかれのてのひらにのせられてすーともちあげられたときなんだかふわふわしたかんじがあったばかりである。てのひらのうえですこしおちついてしょせいのかおをみたのがいわゆるにんげんというもののみはじめであろう。このときみょうなものだとおもったかんじがいまでものこっている。",
+    wakatiRomajiText: "waga hai ha neko dearu . na mae ha ma da na i . do ko de uma re ta ka to n to ken tou ga tu ka nu . nan de mo usu gura i zi me zi me si ta tokoro de nya - nya - na i te i ta koto da ke ha ki oku si te i ru . waga hai ha ko ko de hazi me te nin gen to i u mo no wo mi ta . si ka mo a to de ki ku to so re ha syo sei to i u nin gen juu de iti ban dou aku na syu zoku de a tta so u da . ko no syo sei to i u no ha toki doki ware ware wo tora e te ni te ku u to i u hanasi de a ru . si ka si so no tou zi ha nan to i u kanga e mo na ka tta kara betu dan kowa si i to mo omo wa na ka tta . ta da kare no tenohira ni no se rare te su - to mo ti a ge ra re ta toki nan da ka hu wa hu wa si ta kan zi ga a ta ba ka ri de a ru . tenohira no ue de suko si o ti tu i te syo sei no kao wo mi ta no ga i wa yu ru nin gen to i u mo no no mi ha zi me de a ro u . ko no toki myou na mo no da to omo tta kan zi ga ima de mo noko tte i ru .",
+    romajiText: "wagahaihanekodearu.namaehamadanai.dokodeumaretakatontokentougatukanu.nandemousuguraizimezimesitatokorodenya-nya-naiteitakotodakehakiokusiteiru.wagahaihakokodehazimeteningentoiumonowomita.sikamoatodekikutosorehasyoseitoiuningentyuudeitibandouakunasyuzokudeaxtutasouda.konosyoseitoiunohatokidokiwarewarewotoraetenitekuutoiuhanasidearu.sikasisonotouzihanantoiukangaemonakaxtutakarabetudankowasiitomoomowanakaxtuta.tadakarenotenohiraninoseraretesu-tomotiageraretatokinandakahuwahuwasitakanzigaaxtutabakaridearu.tenohiranouedesukosiotituitesyoseinokaowomitanogaiwayuruningentoiumononomihazimedearou.konotokimyounamonodatoomoxtutakanzigaimademonokoxtuteiru.",
+    url: "https://www.aozora.gr.jp/cards/000148/card789.html"
+  },
+  {
+    id: 624,
+    title: "山月記",
+    author: "中島 敦",
+    kanjiText: "袁傪は叢に向って、懇ろに別れの言葉を述べ、馬に上った。叢の中からは、又、堪え得ざるが如き悲泣の声が洩れた。袁傪も幾度か叢を振返りながら、涙の中に出発した。一行が丘の上についた時、彼等は、言われた通りに振返って、先程の林間の草地を眺めた。忽ち、一匹の虎が草の茂みから道の上に躍り出たのを彼等は見た。虎は、既に白く光を失った月を仰いで、二声三声咆哮したかと思うと、又、元の叢に躍り入って、再びその姿を見なかった。",
+    hiraganaText: "えんさんはくさむらにむかって、ねんごろにわかれの ことば を の べ 、 うま に あが っ た 。 くさむら の なか から は 、 また 、 た え え ざる が ごと き ひきゅう の こえ が も れ た 。 えん さん も いくど か くさむら を ふりかえ り ながら 、 なみだ の なか に しゅっぱつ し た 。 いっこう が おか の うえ に つい た とき 、 かれら は 、 い わ れ た とお り に ふりかえ っ て 、 さきほど の りんかん の くさち を なが め た 。 たちま ち 、 一 ひき の とら が くさ の しげ み から みち の うえ に おど り で た の を かれら は み た 。 とら は 、 すで に しろ く ひかり を うしな っ た つき を あお い で 、 二 こえ 三 こえ ほうこう し た か と おも う と 、 また 、 もと の くさむら に おど り はい っ て 、 ふたた び その すがた を み なかっ た 。",
+    wakatiRomajiText: "en san ha kusamura ni muka tte , nengo ro ni waka re no koto ba wo no be , uma ni aga tta . kusamura no naka ka ra ha , mata , ta e e za ru ga goto ki hi kyuu no koe ga mo re ta . en san mo iku do ka kusamura wo huri kae ri na ga ra , namida no naka ni syu ppatu si ta . i kkou ga oka no ue ni tu i ta toki , kare ra ha , i wa re ta too ri ni huri kae tte , saki hodo no rin kan no kusa ti wo naga me ta . tatima ti , i ppiki no tora ga kusa no sige mi ka ra miti no ue ni odo ri de ta no wo kare ra ha mi ta . tora ha , sude ni siro ku hikari wo usina tta tuki wo ao i de , ni koe san koe houkou si ta ka to omo u to , mata , moto no kusamura ni odo ri hai tte , hutata bi sono sugata wo mi na ka tta .",
+    romajiText: "ensanhakusamuranimukatte,nengoroniwakarenokotobawonobe,umaniagatta.kusamuranonakakaraha,mata,taeezarugagotokihikyuunokoegamoreta.ensanmoikudokakusamurawohurikaerinagara,namidanonakanisyuppatusita.ikkougaokanouenituitatoki,kareraha,iwaretatoorinihurikaette,sakihodonorinkannokusatiwonagameta.tatimati,ippikinotoragakusanosigemikaramitinoueniodoridetanowokarerahamita.toraha,sudenisirokuhikariwousinattatukiwoaoide,nikoesankoehoukousitakatoomouto,mata,motonokusamuraniodorihaitte,hutatabisonosugatawominakatta.",
+    url: "https://www.aozora.gr.jp/cards/000119/files/624_14544.html"
+  },
+  {
+    id: 127,
+    title: "羅生門",
+    author: "芥川 竜之介",
+    kanjiText: "ある日の暮方の事である。一人の下人が、羅生門の下で雨やみを待っていた。広い門の下には、この男のほかに誰もいない。ただ、所々丹塗の剥げた、大きな円柱に、蟋蟀が一匹とまっている。羅生門が、朱雀大路にある以上は、この男のほかにも、雨やみをする市女笠や揉烏帽子が、もう二三人はありそうなものである。それが、この男のほかには誰もいない。",
+    hiraganaText: "あるひのくれがたのことである。ひとりのげにんが、らしょうもんのしたであまやみをまっていた。ひろいもんのしたには、このおとこのほかにだれもいない。ただ、ところどころにぬりのはげた、おおきなえんちゅうに、きりぎりすがいっぴきとまっている。らしょうもんが、すざくおおじにあるいじょうは、このおとこのほかにも、あまやみをするいちめがさやもみえぼしが、もう二三にんはありそうなものである。それが、このおとこのほかにはだれもいない。",
+    wakatiRomajiText: "a ru hi no kure gata no koto de a ru . hito ri no ge nin ga , ra syou mon no sita de ama ya mi wo ma tte i ta . hiro i mon no sita ni ha , ko no otoko no ho ka ni dare mo i nai . ta da , tokoro dokoro ni nuri no ha ge ta , oo ki na en tyuu ni , kiri girisu ga i ppiki to ma tte i ru . ra syou mon ga , su zaku oo zi ni a ru i zyou ha , ko no otoko no ho ka ni mo , ama ya mi wo su ru iti me gasa ya momi e bo si ga , mou ni sann nin ha a ri so u na mo no de a ru . so re ga , ko no otoko no ho ka ni ha dare mo i n ai .",
+    romajiText: "aruhinokuregatanokotodearu.hitorinogeninga,rasyoumonnositadeamayamiwomatteita.hiroimonnositaniha,konootokonohokanidaremoinai.tada,tokorodokoroninurinohageta,ookinaentyuuni,kirigirisugaippikitomatteiru.rasyoumonga,suzakuooziniaruizyouha,konootokonohokanimo,amayamiwosuruitimegasayamomiebosiga,mounisannninhaarisounamonodearu.sorega,konootokonohokanihadaremoinai.",
+    url: "https://www.aozora.gr.jp/cards/000879/files/127_15260.html"
+  },
+]
